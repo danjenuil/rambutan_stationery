ad0f197fbc0a993fd2dac3d4b6180fab462c33c2
Danny Jenuil <dannyroy1002@gmail.com>
2019-02-20 17:56:53 +0800

    Disable seeding database with sample data upon deploying

--- config/deploy.rb
+++ config/deploy.rb
@@ -64,7 +64,7 @@ namespace :deploy do
       within release_path do
         with rails_env: fetch(:rails_env) do
           execute :rake, 'db:migrate:reset'
-          execute :rake, 'db:seed'
+          #execute :rake, 'db:seed'
         end
       end
     end
